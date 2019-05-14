c83e430053092e12b7599d59e2f85432d514ab88
Adhisha <adhishagc@gmail.com>
2019-05-14 21:34:47 +0530

    Re master

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-#Hello World
-This is a sample projects
+#Project Description
--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup
 
-with open("README.md","r") as fh:
-	long_description = fh.read()
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
 
 setup(
-	name='ackleys',
-	version='0.0.1',
-	description ='Ackleys function Generator',
-	py_modules = ["ackleys"],
-	package_dir={'':'src'},
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.6",
-		"Programming Language :: Python :: 3.7",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
-	url="https://github.com/adhishagc/Ackley-Function",
-	author="Adhisha Gammanpila",
-	author_email="adhishagc@gmail.com",
+    name="ackley",
+    version="1.0.8",
+    description="A Python package to generate Ackley Function values.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/adhishagc/Ackley-Function",
+    author="Adhisha Gammanpila",
+    author_email="adhishagc@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["numpy"]
+    
 )
