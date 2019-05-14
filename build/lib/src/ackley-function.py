e4b79cf6e32e2dddeff38bc0772ebccd332fa3af
Adhisha <adhishagc@gmail.com>
2019-05-14 21:40:36 +0530

    Version 1.0.8

---
+++ build/lib/src/ackley-function.py
@@ -0,0 +1,19 @@
+#importing libraries
+import numpy as np
+import math
+
+
+
+def ackley_function(x_range_array):
+  #returns an array of values for the given x range of values
+  value = np.empty([len(x_range_array[0])])
+  for i in range(len(x_range_array[0])):
+    
+    #returns the point value of the given coordinate
+    part_1 = -0.2*math.sqrt(0.5*(x_range_array[0][i]*x_range_array[0][i] + x_range_array[1][i]*x_range_array[1][i]))
+    part_2 = 0.5*(math.cos(2*math.pi*x_range_array[0][i]) + math.cos(2*math.pi*x_range_array[1][i]))
+    
+    value_point = math.exp(1) + 20 -20*math.exp(part_1) - math.exp(part_2)
+    value[i] = value_point
+  #returning the value array
+  return value
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="ackley",
+    version="1.0.8",
+    description="A Python package to generate Ackley Function values.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/adhishagc/Ackley-Function",
+    author="Adhisha Gammanpila",
+    author_email="adhishagc@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["numpy"]
+    
+)
