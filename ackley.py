29dd3c968bdcf41af5a920cf55c6ddb1cf9dd64b
Adhisha Gammanpila <adhishagc@gmail.com>
2019-05-15 19:37:31 +0530

    Fixed the 2 function input enhancement issue #1

--- ackley.py
+++ ackley.py
@@ -2,9 +2,15 @@
 import numpy as np
 import math
 
+def ackley_function(x1,x2):
+  #returns the point value of the given coordinate
+  part_1 = -0.2*math.sqrt(0.5*(x1*x1 + x2*x2))
+  part_2 = 0.5*(math.cos(2*math.pi*x1) + math.cos(2*math.pi*x2))
+  value = math.exp(1) + 20 -20*math.exp(part_1) - math.exp(part_2)
+  #returning the value
+  return value
 
-
-def ackley_function(x_range_array):
+def ackley_function_range(x_range_array):
   #returns an array of values for the given x range of values
   value = np.empty([len(x_range_array[0])])
   for i in range(len(x_range_array[0])):
